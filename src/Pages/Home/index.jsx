a4f3304d4cdae2874d4c2fdb22b08ea6faab1179
mike-droid <miguelreyesmoreno@hotmail.com>
2023-05-10 20:11:37 -0600

    Clase 8

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 
+import { apiUrl } from '../../api/'
+
 export const Home = () => {
+  const [Items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`);
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card></Card>
+      <section className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          Items?.map(item => (
+            <Card
+              key={item.id}
+              data={item}
+            />
+          ))
+        }
+      </section>
     </Layout>
   )
 }
