df29d005384b9a87dab6fc50f15bf26b3264406d
mike-droid <miguelreyesmoreno@hotmail.com>
2023-05-12 13:35:10 -0600

    clase 18

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import { OrderCard } from '../OrderCard'
 
 import './styles.css'
 
@@ -18,6 +19,19 @@ export const CheckoutSideMenu = () => {
             <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
           </svg>
         </button>
+      </div>
+      <div className='px-6'>
+        {
+          context.CartProducts.map((product) => (
+              <OrderCard
+                key={product.id}
+                title={product.title}
+                imageUrl={product.images[0]}
+                price={product.price}
+              />
+            )
+          )
+        }
       </div>
     </aside>
   )
